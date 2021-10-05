0379c24d4f71b0802778393c9aa23e1ac5e64e78
Gabriel Pizarro <gpizarro@javaman.net>
2021-10-05 06:41:31 -0700

    Submodules 4

--- src/main/java/gg/sbs/datapuller/tasks/HypixelResourceTask.java
+++ src/main/java/gg/sbs/datapuller/tasks/HypixelResourceTask.java
@@ -1,5 +1,7 @@
 package gg.sbs.datapuller.tasks;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import gg.sbs.api.SimplifiedApi;
 import gg.sbs.api.apiclients.hypixel.implementation.HypixelResourceData;
 import gg.sbs.api.apiclients.hypixel.response.ResourceCollectionsResponse;
@@ -15,8 +17,12 @@ import gg.sbs.api.data.sql.models.collectionitemtiers.CollectionItemTierReposito
 import gg.sbs.api.data.sql.models.collections.CollectionModel;
 import gg.sbs.api.data.sql.models.collections.CollectionRefreshable;
 import gg.sbs.api.data.sql.models.collections.CollectionRepository;
+import gg.sbs.api.data.sql.models.items.ItemModel;
 import gg.sbs.api.data.sql.models.items.ItemRefreshable;
 import gg.sbs.api.data.sql.models.items.ItemRepository;
+import gg.sbs.api.data.sql.models.rarities.RarityModel;
+import gg.sbs.api.data.sql.models.rarities.RarityRefreshable;
+import gg.sbs.api.data.sql.models.rarities.RarityRepository;
 import gg.sbs.api.data.sql.models.skilllevels.SkillLevelModel;
 import gg.sbs.api.data.sql.models.skilllevels.SkillLevelRefreshable;
 import gg.sbs.api.data.sql.models.skilllevels.SkillLevelRepository;
@@ -25,6 +31,8 @@ import gg.sbs.api.data.sql.models.skills.SkillRefreshable;
 import gg.sbs.api.data.sql.models.skills.SkillRepository;
 import gg.sbs.api.util.Pair;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class HypixelResourceTask {
@@ -34,13 +42,16 @@ public class HypixelResourceTask {
     private static final CollectionItemRefreshable collectionItemRefreshable;
     private static final CollectionItemTierRefreshable collectionItemTierRefreshable;
     private static final ItemRefreshable itemRefreshable;
+    private static final RarityRefreshable rarityRefreshable;
     private static final SkillRepository skillRepository;
     private static final SkillLevelRepository skillLevelRepository;
     private static final CollectionRepository collectionRepository;
     private static final CollectionItemRepository collectionItemRepository;
     private static final CollectionItemTierRepository collectionItemTierRepository;
     private static final ItemRepository itemRepository;
+    private static final RarityRepository rarityRepository;
     private static final HypixelResourceData hypixelResourceData;
+    private static final Gson gson;
 
     static {
         skillRefreshable = SimplifiedApi.getSqlRefreshable(SkillRefreshable.class);
@@ -49,13 +60,16 @@ public class HypixelResourceTask {
         collectionItemRefreshable = SimplifiedApi.getSqlRefreshable(CollectionItemRefreshable.class);
         collectionItemTierRefreshable = SimplifiedApi.getSqlRefreshable(CollectionItemTierRefreshable.class);
         itemRefreshable = SimplifiedApi.getSqlRefreshable(ItemRefreshable.class);
+        rarityRefreshable = SimplifiedApi.getSqlRefreshable(RarityRefreshable.class);
         skillRepository = SimplifiedApi.getSqlRepository(SkillRepository.class);
         skillLevelRepository = SimplifiedApi.getSqlRepository(SkillLevelRepository.class);
         collectionRepository = SimplifiedApi.getSqlRepository(CollectionRepository.class);
         collectionItemRepository = SimplifiedApi.getSqlRepository(CollectionItemRepository.class);
         collectionItemTierRepository = SimplifiedApi.getSqlRepository(CollectionItemTierRepository.class);
         itemRepository = SimplifiedApi.getSqlRepository(ItemRepository.class);
+        rarityRepository = SimplifiedApi.getSqlRepository(RarityRepository.class);
         hypixelResourceData = SimplifiedApi.getWebApi(HypixelResourceData.class);
+        gson = new GsonBuilder().create();
     }
 
     public static long getFixedRateMs() {
@@ -82,6 +96,9 @@ public class HypixelResourceTask {
                     }
                 }
             }
+            for (ResourceItemsResponse.Item item : items.getItems()) {
+                updateItem(item);
+            }
         } catch (SqlException e) {
             e.printStackTrace();
         }
@@ -92,8 +109,8 @@ public class HypixelResourceTask {
                 SkillModel::getSkillKey, key
         );
         if (existingSkill != null) {
-            if (!(existingSkill.getName().equals(skill.getName())
-                    && existingSkill.getDescription().equals(skill.getDescription())
+            if (!(equalsWithNull(existingSkill.getName(), skill.getName())
+                    && equalsWithNull(existingSkill.getDescription(), skill.getDescription())
                     && existingSkill.getMaxLevel() == skill.getMaxLevel()
             )) {
                 existingSkill.setName(skill.getName());
@@ -122,7 +139,7 @@ public class HypixelResourceTask {
                 new Pair<>(SkillLevelModel::getSkillLevel, skillLevel.getLevel())
         );
         if (existingSkillLevel != null) {
-            if (!(existingSkillLevel.getUnlocks().equals(skillLevel.getUnlocks())
+            if (!(equalsWithNull(existingSkillLevel.getUnlocks(), skillLevel.getUnlocks())
                     && existingSkillLevel.getTotalExpRequired() == skillLevel.getTotalExpRequired()
             )) {
                 existingSkillLevel.setUnlocks(skillLevel.getUnlocks());
@@ -141,12 +158,12 @@ public class HypixelResourceTask {
         }
     }
 
-    private static CollectionModel updateCollection(ResourceCollectionsResponse.Collection collection, String key) throws  SqlException {
+    private static CollectionModel updateCollection(ResourceCollectionsResponse.Collection collection, String key) throws SqlException {
         CollectionModel existingCollection = collectionRefreshable.findFirstOrNull(
                 CollectionModel::getCollectionKey, key
         );
         if (existingCollection != null) {
-            if (!(existingCollection.getName().equals(collection.getName()))) {
+            if (!(equalsWithNull(existingCollection.getName(), collection.getName()))) {
                 existingCollection.setName(collection.getName());
                 collectionRepository.update(existingCollection);
                 collectionRefreshable.refreshItems();
@@ -169,7 +186,7 @@ public class HypixelResourceTask {
                 new Pair<>(CollectionItemModel::getItemKey, key)
         );
         if (existingCollectionItem != null) {
-            if (!(existingCollectionItem.getName().equals(collectionItem.getName())
+            if (!(equalsWithNull(existingCollectionItem.getName(), collectionItem.getName())
                     && existingCollectionItem.getMaxTiers() == collectionItem.getMaxTiers()
             )) {
                 existingCollectionItem.setName(collectionItem.getName());
@@ -197,7 +214,7 @@ public class HypixelResourceTask {
                 new Pair<>(CollectionItemTierModel::getTier, collectionTier.getTier())
         );
         if (existingCollectionTier != null) {
-            if (!(existingCollectionTier.getUnlocks().equals(collectionTier.getUnlocks())
+            if (!(equalsWithNull(existingCollectionTier.getUnlocks(), collectionTier.getUnlocks())
                     && existingCollectionTier.getAmountRequired() == collectionTier.getAmountRequired()
             )) {
                 existingCollectionTier.setUnlocks(collectionTier.getUnlocks());
@@ -214,5 +231,127 @@ public class HypixelResourceTask {
             collectionItemTierRepository.save(newCollectionTier);
             collectionItemTierRefreshable.refreshItems();
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void updateItem(ResourceItemsResponse.Item item) throws SqlException {
+        ItemModel existingItem = itemRefreshable.findFirstOrNull(
+                ItemModel::getItemId, item.getId()
+        );
+        Map<String, Object> requirements = gson.fromJson(gson.toJson(item.getRequirements()), HashMap.class);
+        Map<String, Object> catacombsRequirements = gson.fromJson(gson.toJson(item.getCatacombsRequirements()), HashMap.class);
+        Map<String, Object> essence = gson.fromJson(gson.toJson(item.getEssence()), HashMap.class);
+        if (item.getTier() != null) {
+            RarityModel existingRarity = rarityRefreshable.findFirstOrNull(
+                    new Pair<>(RarityModel::getHypixelName, item.getTier()),
+                    new Pair<>(RarityModel::isHasHypixelName, true)
+            );
+            if (existingRarity == null) {
+                RarityModel newRarity = new RarityModel();
+                newRarity.setName(item.getTier().substring(0, 1).toUpperCase()
+                        + item.getTier().substring(1).toLowerCase()
+                );
+                newRarity.setHasHypixelName(true);
+                newRarity.setHypixelName(item.getTier());
+                rarityRepository.save(newRarity);
+                rarityRefreshable.refreshItems();
+            }
+        }
+        RarityModel rarity = rarityRefreshable.findFirstOrNull(
+                new Pair<>(RarityModel::getHypixelName, item.getTier()),
+                new Pair<>(RarityModel::isHasHypixelName, true)
+        );
+        if (existingItem != null) {
+            if (!(equalsWithNull(existingItem.getName(), item.getName())
+                    && equalsWithNull(existingItem.getMaterial(), item.getMaterial())
+                    && existingItem.getDurability() == item.getDurability()
+                    && equalsWithNull(existingItem.getSkin(), item.getSkin())
+                    && equalsWithNull(existingItem.getFurniture(), item.getFurniture())
+                    && equalsWithNull(existingItem.getRarity(), rarity)
+                    && equalsWithNull(existingItem.getItemId(), item.getId())
+                    && equalsWithNull(existingItem.getGenerator(), item.getGenerator())
+                    && existingItem.getGeneratorTier() == item.getGeneratorTier()
+                    && existingItem.isGlowing() == item.isGlowing()
+                    && equalsWithNull(existingItem.getStats(), item.getStats())
+                    && existingItem.getNpcSellPrice() == item.getNpcSellPrice()
+                    && existingItem.isUnstackable() == item.isUnstackable()
+                    && equalsWithNull(existingItem.getColor(), item.getColor())
+                    && equalsWithNull(existingItem.getTieredStats(), item.getTieredStats())
+                    && existingItem.getGearScore() == item.getGearScore()
+                    && equalsWithNull(existingItem.getRequirements(), requirements)
+                    && equalsWithNull(existingItem.getCatacombsRequirements(), catacombsRequirements)
+                    && equalsWithNull(existingItem.getEssence(), essence)
+                    && equalsWithNull(existingItem.getDescription(), item.getDescription())
+                    && existingItem.getAbilityDamageScaling() == item.getAbilityDamageScaling()
+                    && equalsWithNull(existingItem.getEnchantments(), item.getEnchantments())
+                    && equalsWithNull(existingItem.getCrystal(), item.getCrystal())
+                    && equalsWithNull(existingItem.getPrivateIsland(), item.getPrivateIsland())
+                    && equalsWithNull(existingItem.getCategory(), item.getCategory())
+            )) {
+                existingItem.setName(item.getName());
+                existingItem.setMaterial(item.getMaterial());
+                existingItem.setDurability(item.getDurability());
+                existingItem.setSkin(item.getSkin());
+                existingItem.setFurniture(item.getFurniture());
+                existingItem.setRarity(rarity);
+                existingItem.setItemId(item.getId());
+                existingItem.setGenerator(item.getGenerator());
+                existingItem.setGeneratorTier(item.getGeneratorTier());
+                existingItem.setGlowing(item.isGlowing());
+                existingItem.setStats(item.getStats());
+                existingItem.setNpcSellPrice(item.getNpcSellPrice());
+                existingItem.setUnstackable(item.isUnstackable());
+                existingItem.setColor(item.getColor());
+                existingItem.setTieredStats(item.getTieredStats());
+                existingItem.setGearScore(item.getGearScore());
+                existingItem.setRequirements(requirements);
+                existingItem.setCatacombsRequirements(catacombsRequirements);
+                existingItem.setEssence(essence);
+                existingItem.setDescription(item.getDescription());
+                existingItem.setAbilityDamageScaling(item.getAbilityDamageScaling());
+                existingItem.setEnchantments(item.getEnchantments());
+                existingItem.setCrystal(item.getCrystal());
+                existingItem.setPrivateIsland(item.getPrivateIsland());
+                existingItem.setCategory(item.getCategory());
+                itemRepository.update(existingItem);
+                itemRefreshable.refreshItems();
+            }
+        } else {
+            ItemModel newItem = new ItemModel();
+            newItem.setItemId(item.getId());
+            newItem.setName(item.getName());
+            newItem.setMaterial(item.getMaterial());
+            newItem.setDurability(item.getDurability());
+            newItem.setSkin(item.getSkin());
+            newItem.setFurniture(item.getFurniture());
+            newItem.setRarity(rarity);
+            newItem.setItemId(item.getId());
+            newItem.setGenerator(item.getGenerator());
+            newItem.setGeneratorTier(item.getGeneratorTier());
+            newItem.setGlowing(item.isGlowing());
+            newItem.setStats(item.getStats());
+            newItem.setNpcSellPrice(item.getNpcSellPrice());
+            newItem.setUnstackable(item.isUnstackable());
+            newItem.setColor(item.getColor());
+            newItem.setTieredStats(item.getTieredStats());
+            newItem.setGearScore(item.getGearScore());
+            newItem.setRequirements(requirements);
+            newItem.setCatacombsRequirements(catacombsRequirements);
+            newItem.setEssence(essence);
+            newItem.setDescription(item.getDescription());
+            newItem.setAbilityDamageScaling(item.getAbilityDamageScaling());
+            newItem.setEnchantments(item.getEnchantments());
+            newItem.setCrystal(item.getCrystal());
+            newItem.setPrivateIsland(item.getPrivateIsland());
+            newItem.setCategory(item.getCategory());
+            itemRepository.saveOrUpdate(newItem);
+            itemRefreshable.refreshItems();
+        }
+    }
+
+    private static boolean equalsWithNull(Object a, Object b) {
+        if (a == b) return true;
+        if (a == null || b == null) return false;
+        return a.equals(b);
     }
 }
